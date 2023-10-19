a4f4f4869c90f6929a77ddef5cd887137242ce8d
surovishimu <surovi.shimu95@gmail.com>
2023-10-19 11:26:24 +0600

    delete operation for delete cart data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -43,7 +43,14 @@ async function run() {
             console.log(result);
             res.send(result);
         })
-
+        app.delete('/mycart/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id),
+            }
+            const result=await cartProductCollection.deleteOne(query);
+            res.send(result)
+        })
 
 
         // product db
